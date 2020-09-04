19e7a2d2f8a4691d710157b8a1bb9efb87154663
Unknown <geoffreydu38@hotmail.fr>
2020-09-04 22:26:08 +0200

    Fix + add command update anime
    Bot was KO after the update of ytdl

---
+++ docs/installscript.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install curl
+
+# install node
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+# install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt update
+
+sudo apt install nodejs
+sudo apt install yarn
+
+#git
+sudo apt install git
+
+#ffmpeg
+sudo apt install ffmpeg
+
+#bot discord
+mkdir discordBot
+
+cd ./discordBot
+
+git clone https://github.com/Aimiks/thomas_bot.git
+
+cd ./thomas_bot
+
+yarn install
+
+echo module.exports = {  >> sensitive_infos.js
+echo BOT_TOKEN:\"MzE4Nzc1NDgxNTE0MDY1OTIw.Dt3FpA.16YZ2LRE8_L6plT7UoyoSCYZ6Rk\", >> sensitive_infos.js
+echo YT_KEY:\"AIzaSyAflLeBJij8VRe_Reh8IgOyzBGpac6_dHo\" >> sensitive_infos.js
+echo } >> sensitive_infos.js
+
--- main.js
+++ main.js
@@ -1,32 +1,30 @@
-const { BOT_TOKEN } = require('./sensitive_infos');
-const { YT_KEY } = require('./sensitive_infos.js');
+const { BOT_TOKEN } = require("./sensitive_infos");
+const { YT_KEY } = require("./sensitive_infos.js");
 
-const Discord = require('discord.js'); // Require the Discord.js library.
-const ytdl = require('ytdl-core');
+const Discord = require("discord.js"); // Require the Discord.js library.
+const ytdl = require("ytdl-core");
 const commands = require("./commands");
-const Partie = require('./model/Partie.js');
-const Bot = require('./model/Bot.js');
-
-
+const Partie = require("./model/Partie.js");
+const Bot = require("./model/Bot.js");
 
 //var mpTable = ["Lewho", "「Mx」"];
 /** @type Partie  */
 var Game = null;
 
-
 const client = new Bot();
 client.music.start(client, {
-    botPrefix: ">",
-    youtubeKey: YT_KEY, // Set the api key used for YouTube.
-    cooldown: {
-        disabled: true
-    }
+  botPrefix: ">",
+  youtubeKey: YT_KEY, // Set the api key used for YouTube.
+  cooldown: {
+    disabled: true,
+  },
 });
 client.login(BOT_TOKEN);
-client.on('ready', function () {
-    console.log(`Je suis connecté à ${client.guilds.array().length} serveur(s) !`);
+client.on("ready", function () {
+  console.log(
+    `Je suis connecté à ${client.guilds.array().length} serveur(s) !`
+  );
 });
-
 
 ///////////////
 /////////Commandes play/switch/pause/unpause
@@ -36,129 +34,120 @@ var streamOptions;
 var stream;
 var conn;
 // Music
-client.on('message', (message) => {
-    if (message.content.startsWith('>playL ')) {
-        let link = message.content.split(' ')[1];
-        //const streamOptions = { seek: 0, volume: 1 };
-        var voiceChannel = message.member.voiceChannel;
-        voiceChannel.join().then(connection => {
-            conn = connection;
-            console.log("joined channel");
-            streamOptions = { seek: 0, volume: 1 };
-            stream = ytdl(link, { filter: 'audioonly' });
-            dispatcher = connection.playStream(stream, streamOptions);
-        }).catch(err => console.log(err));
-    } else if (message.content.startsWith('>pauseL')) {
-        dispatcher.pause();
-    } else if (message.content.startsWith('>unpauseL')) {
-        dispatcher.resume();
-    } else if (message.content.startsWith('>switchOnL ')) {
-        let arg = message.content.split(' ')[1];
+// SHITTY DO NOT USE
+client.on("message", (message) => {
+  if (message.content.startsWith(">playL ")) {
+    let link = message.content.split(" ")[1];
+    //const streamOptions = { seek: 0, volume: 1 };
+    var voiceChannel = message.member.voiceChannel;
+    voiceChannel
+      .join()
+      .then((connection) => {
+        conn = connection;
+        console.log("joined channel");
         streamOptions = { seek: 0, volume: 1 };
-        stream = ytdl(link, { filter: 'audioonly' });
-        dispatcher = conn.playStream(stream, streamOptions);
-    }
+        stream = ytdl(link, { filter: "audioonly" });
+        dispatcher = connection.playStream(stream, streamOptions);
+      })
+      .catch((err) => console.log(err));
+  } else if (message.content.startsWith(">pauseL")) {
+    dispatcher.pause();
+  } else if (message.content.startsWith(">unpauseL")) {
+    dispatcher.resume();
+  } else if (message.content.startsWith(">switchOnL ")) {
+    let arg = message.content.split(" ")[1];
+    streamOptions = { seek: 0, volume: 1 };
+    stream = ytdl(link, { filter: "audioonly" });
+    dispatcher = conn.playStream(stream, streamOptions);
+  }
 });
 
-
-
-client.on('message', (message) => {
-    if (message.author.bot) { return; }
-    let noLowerCaseMessage = message.content;
-    message.content = message.content.toLowerCase();
-    /** PRIVATE MESSAGE */
-    if (message.guild === null) {
-
-        /** ANSWER TO PRIVATE MESSAGE WHILE IN GAME */
-        if (Game !== null) {
-            commands.blindTest.privateMessage(message, Game, client);
-        }
-        /** TEST ROLL */
-        if (message.content.startsWith(">roll")) {
-            //seedrandom("27");
-            message.author.send(Math.random());
-        }
-
+client.on("message", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  let noLowerCaseMessage = message.content;
+  message.content = message.content.toLowerCase();
+  /** PRIVATE MESSAGE */
+  if (message.guild === null) {
+    /** ANSWER TO PRIVATE MESSAGE WHILE IN GAME */
+    if (Game !== null) {
+      commands.blindTest.privateMessage(message, Game, client);
     }
-    /** NO PRIVATE MESSAGE */
-    else {
-        /** PLAY BLINDTEST */
-        if (message.content.startsWith(commands.blindTest.prefix.play)) {
-            let noRounds = message.content.split(' ')[1];
-            let seed = message.content.split(' ')[2];
-            if (parseInt(noRounds) > 300) {
-                message.channel.send("Nombre de round trop élévé, max: 300");
-                return;
-            }
-            Game = new Partie(noRounds, seed);
-            commands.blindTest.play(message, Game, client);
-            client.user.setActivity("BlindTest Anime", { type: "PLAYING" });
+    /** TEST ROLL */
+    if (message.content.startsWith(">roll")) {
+      //seedrandom("27");
+      message.author.send(Math.random());
+    }
+  } else {
+    try {
+      /** NO PRIVATE MESSAGE */
+      /** PLAY BLINDTEST */
+      if (message.content.startsWith(commands.blindTest.prefix.play)) {
+        let noRounds = message.content.split(" ")[1];
+        let seed = message.content.split(" ")[2];
+        if (parseInt(noRounds) > 300) {
+          message.channel.send("Nombre de round trop élévé, max: 300");
+          return;
         }
+        Game = new Partie(noRounds, seed);
+        commands.blindTest.play(message, Game, client);
+        client.user.setActivity("BlindTest Anime", { type: "PLAYING" });
+      } else if (message.content.startsWith(commands.blindTest.prefix.add)) {
         /** ADD TO BLINDTEST */
-        else if (message.content.startsWith(commands.blindTest.prefix.add)) {
-            commands.blindTest.add(client, message, YT_KEY);
-        }
+        commands.blindTest.add(client, message, YT_KEY);
+      } else if (
+        message.content.startsWith(commands.blindTest.prefix.replace)
+      ) {
         /** REPLACE ANIME SONG LINK IN BLINDTEST  */
-        else if (message.content.startsWith(commands.blindTest.prefix.replace)) {
-            message.content = noLowerCaseMessage;
-            commands.blindTest.replaceLink(message);
-        }
+        message.content = noLowerCaseMessage;
+        commands.blindTest.replaceLink(message);
+      } else if (message.content.startsWith(commands.blindTest.prefix.remove)) {
         /** REMOVE ANIME IN BLINDTEST */
-        else if (message.content.startsWith(commands.blindTest.prefix.remove)) {
-            commands.blindTest.remove(message);
-        }
+        commands.blindTest.remove(message);
+      } else if (message.content.startsWith(commands.blindTest.prefix.count)) {
         /** SEND SOME OCCURENCES NUMBER IN THE LIST */
-        else if (message.content.startsWith(commands.blindTest.prefix.count)) {
-            commands.blindTest.countList(message.channel);
-        }
-        else if(message.content.startsWith(">bthelp")) {
-            commands.blindTest.help(message);
-        } else if(message.content.startsWith(">btclear")) {
-            commands.blindTest.clearInvalid(message);
-        }
-
+        commands.blindTest.countList(message.channel);
+      } else if (message.content.startsWith(">bthelp")) {
+        commands.blindTest.help(message);
+      } else if (message.content.startsWith(commands.blindTest.prefix.clear)) {
+        commands.blindTest.clearInvalids(message);
+      } else if (message.content.startsWith(commands.blindTest.prefix.update)) {
+        commands.blindTest.updateAnimes(client, message, YT_KEY);
+      } else if (message.content.startsWith(">test")) {
         /** TEST COMMANDS */
         /** TEST X */
-        else if (message.content.startsWith(">test")) {
-            commands.blindTest.util.unserializeAnimeList( (animes) => {
-                commands.blindTest.util.getUniqueAnimeList(animes).then((string) => {
-                    if (string.length > 2000) {
-                        for (let i = 0; i < string.length / 2000; i++) {
-                            message.channel.send(string.substring(i * 2000, (i + 1) * 2000));
-                        }
-                    } else {
-                        message.channel.send(string);
-                    }
-    
-                });
-                message.channel.send("En cours de test...");
-            });
-        }
+        commands.blindTest.util.unserializeAnimeList((animes) => {
+          commands.blindTest.util.getUniqueAnimeList(animes).then((string) => {
+            if (string.length > 2000) {
+              for (let i = 0; i < string.length / 2000; i++) {
+                message.channel.send(
+                  string.substring(i * 2000, (i + 1) * 2000)
+                );
+              }
+            } else {
+              message.channel.send(string);
+            }
+          });
+          message.channel.send("En cours de test...");
+        });
+      }
+    } catch (err) {}
+  }
+});
+
+// Play "false brother" music for the most FF guy in the discord
+client.on("voiceStateUpdate", (oldMember, newMember) => {
+  let newUserChannel = newMember.voiceChannel;
+  let oldUserChannel = oldMember.voiceChannel;
+
+  if (oldUserChannel === undefined && newUserChannel !== undefined) {
+    // User join a voice channel
+
+    if (!newMember.user.bot && newMember.user.username === "Legitness") {
+      client.playFF(newUserChannel);
     }
-
+  } else if (newUserChannel === undefined) {
+    // User leaves a voice channel
+  }
 });
-
-client.on('message', (message) => {
-    
-});
-
-
-
-
-//
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-    let newUserChannel = newMember.voiceChannel
-    let oldUserChannel = oldMember.voiceChannel
-
-    if (oldUserChannel === undefined && newUserChannel !== undefined) {
-        // User join a voice channel
-
-        if (!newMember.user.bot && newMember.user.username === "Legitness") {
-            client.playFF(newUserChannel);
-        }
-
-    } else if (newUserChannel === undefined) {
-        // User leaves a voice channel
-    }
-});
-
