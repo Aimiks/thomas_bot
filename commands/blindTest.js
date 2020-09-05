7f5d4bd211c8f26ea903a0a656851df1d7148d2a
Unknown <geoffreydu38@hotmail.fr>
2020-09-05 19:48:36 +0200

    endboard result out of game model

--- commands/blindTest.js
+++ commands/blindTest.js
@@ -573,8 +573,9 @@ function startNewRound(Game, client) {
         startNewRound(Game, client);
       } else {
         Game.reset();
-        Game.sendEndBoardResult(client);
+        sendEndBoardResult(Game, client);
       }
+      return;
     });
     //let stream = Game.listReadableStreamSongs[Game.curRound];
     /*stream
@@ -812,7 +813,8 @@ module.exports.privateMessage = (message, Game, client) => {
     if (Game.playersHaveResponded) {
       Game.reset();
       if (Game.curRound === Game.noRounds - 1) {
-        Game.sendEndBoardResult(client);
+        sendEndBoardResult(Game, client);
+        return;
       } else {
         Game.curRound++;
         startNewRound(Game, client);
@@ -1009,4 +1011,72 @@ function toTitleCase(str) {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
+}
+
+function sendEndBoardResult(Game, client) {
+  let idWinner = Game.getBestPlayerScore();
+  let nameWinner = Game.getPlayerUserName(idWinner);
+  let scoreWinner = Game.getPlayerScore(idWinner);
+  let mptabtemp = Game.getAllPlayersUser().slice();
+  /**@type {Player[]} */
+  let finalBoard = Game.getEndBoardResult();
+  let opts_embed = {
+    title: `:headphones: Le blind test est finit !`,
+    description: `Le seed de la partie était : ${Game.ID}`,
+    color: client.resolver.resolveColor("RANDOM"),
+  };
+  let final_embed_message = new Discord.RichEmbed(opts_embed);
+  final_embed_message.setThumbnail(Game.getPlayerUser(idWinner).avatarURL);
+  let emojis = [
+    ":trophy:",
+    ":second_place:",
+    ":third_place:",
+    ":slight_smile:",
+    ":slight_smile:",
+    ":neutral_face:",
+    ":neutral_face:",
+    ":neutral_face:",
+    ":neutral_face:",
+    ":neutral_face:",
+  ];
+  let default_emoji = ":worried:";
+  emojis = emojis.fill(default_emoji, 10, 25);
+  final_embed_message.addField(
+    `:trophy: **${nameWinner}** :trophy:`,
+    `**Score** : __${scoreWinner.toFixed(
+      2
+    )}__ :small_orange_diamond:\n**Meilleur score** : __${finalBoard[0].bestScore.toFixed(
+      2
+    )}__ sur __${finalBoard[0].bestSong}__ trouvé en ${
+      finalBoard[0].time
+    } secondes`
+  );
+  for (let index = 1; index < finalBoard.length && index < 24; index++) {
+    let stats = "";
+    if (index < 6) {
+      stats = `\n**Meilleur score** : __${finalBoard[index].bestScore.toFixed(
+        2
+      )}__ sur __${finalBoard[index].bestSong}__ trouvé en ${
+        finalBoard[index].time
+      } secondes`;
+    }
+    if (index === 23) {
+      final_embed_message.addField("...", "...");
+    } else if (index < 23) {
+      final_embed_message.addField(
+        `${emojis[index]} ${finalBoard[index].username}`,
+        `Score : __${finalBoard[index].score.toFixed(
+          2
+        )}__ :small_orange_diamond:${stats}`,
+        true
+      );
+    }
+  }
+
+  mptabtemp.forEach((e) => {
+    e.send(final_embed_message);
+  });
+  Game.started = false;
+  Game.voiceChannel.leave();
+  client.user.setActivity("");
 }
